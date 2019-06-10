3afad16439ee646cdc3305ee2ff3acc120664b90
Ilya <neovlad96@gmail.com>
2019-06-10 14:11:20 +0300

    Updates

---
+++ precache-manifest.c25d44a458e14cd154e14949f1f697ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c77fb212c773183178e91ac10dd017d",
+    "url": "/weater/index.html"
+  },
+  {
+    "revision": "05fe54140fa7824ad07e",
+    "url": "/weater/static/js/2.69aa27fd.chunk.js"
+  },
+  {
+    "revision": "7e496eb4f3d2091bae51",
+    "url": "/weater/static/js/main.6aace442.chunk.js"
+  },
+  {
+    "revision": "51692fc6fc451b8eff8f",
+    "url": "/weater/static/js/runtime~main.e6f24589.js"
+  }
+]);
